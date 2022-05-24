535f53fad28acdf28d82cb13693a764e9c503a50
byteErudite <raghuvanshivibhu@gmail.com>
2022-05-24 15:42:55 +0530

    Added initial project structure

---
+++ src/main/java/com/vaibhav/graphQL/springbootgraphQLservice/SpringBootGraphQlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhav.graphQL.springbootgraphQLservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphQlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphQlServiceApplication.class, args);
+	}
+
+}
